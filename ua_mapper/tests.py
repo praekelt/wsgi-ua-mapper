7278f08ead22f7485da7dd1efaa0c5a4b23d3edc
hedleyroos <hedleyroos@gmail.com>
2011-11-21 14:10:35 +0200

    Add test demonstrating failure on some user agents

--- ua_mapper/tests.py
+++ ua_mapper/tests.py
@@ -1,14 +1,17 @@
+import os
 import unittest
 import httplib
 from threading import Thread
 from wsgiref.simple_server import make_server, demo_app
 
-from wsgi import UAMapper
+import wsgi
+
+_COUNTER = 0
 
 
 class HttpdThread(Thread):
     """Threading class so WSGI server can run in background"""
-    
+
     def __init__(self, httpd, *args, **kwargs):
         self.httpd = httpd
         self._terminated = False
@@ -25,24 +28,36 @@ class HttpdThread(Thread):
 class TestWSGI(unittest.TestCase):
 
     def setUp(self):
-        self.mapper = UAMapper()
+        global _COUNTER
+        self.port = 8080 + _COUNTER
+        _COUNTER += 1
+
+        self.mapper = wsgi.UAMapper()
         self.extra_environ = dict(
             MEMCACHED_SOCKET='127.0.0.1:11211',
             HTTP_USER_AGENT=''
         )
-
-    def test_responses(self):
-
         def app(environ, start_response):
             environ.update(self.extra_environ)
             result = self.mapper(environ, start_response)
             return result
 
-        thread = HttpdThread(make_server('', 8080, app))
-        thread.start()
+        # WSGI server takes some time to die, so start each test on a new port.
+        self.httpd_thread = HttpdThread(make_server('', self.port, app))
+        self.httpd_thread.start()
+
+    def tearDown(self):
+        self.httpd_thread.terminate()
+
+        # Lame hack required since WSGI server does not like stdout being
+        # yanked from underneath it.
+        from time import sleep
+        sleep(1)
+
+    def test_responses(self):
 
         # Make HTTP request for levels of devices
-        conn = httplib.HTTPConnection('127.0.0.1', 8080)
+        conn = httplib.HTTPConnection('127.0.0.1', self.port)
     
         # Medium
         self.extra_environ['HTTP_USER_AGENT'] = 'Nokia3100/1.0 (02.70) Profile/MIDP-1.0 Configuration/CLDC-1.0'
@@ -58,13 +73,25 @@ class TestWSGI(unittest.TestCase):
 
         # Clean up
         conn.close()
-        thread.terminate()
 
-        # Warning - lameness alert. WSGI server wants to log all requests and 
-        # we cannot yank stdout from underneath it. There must be a better way.
-        from time import sleep
-        sleep(1)
+    def test_user_agents(self):
 
+        pth = os.path.split(wsgi.__file__)[0]
+        fp = open(pth + '/useragents.txt', 'r')
+        try:
+            lines = fp.readlines()
+        finally:
+            fp.close()
+
+        conn = httplib.HTTPConnection('127.0.0.1', self.port)
+        for line in lines:
+            self.extra_environ['HTTP_USER_AGENT'] = line
+            conn.request('GET', '/some/url')
+            result = conn.getresponse().read()
+            print result
+            #self.assertEquals(result, 'medium')
+
+        conn.close()
 
 if __name__ == '__main__':
     unittest.main()
